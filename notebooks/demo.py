03b0a67ccc0a1555d987398490e7b72e91004357
Luis M. Torres-Villegas <lmtorresv@eafit.edu.co>
2026-04-21 16:14:28 -0500

    Add explore plot with Folium
    I'm looking at this Jupyter notebook:
    <https://github.com/gboeing/osmnx-examples/blob/main/notebooks/11-interactive-web-mapping.ipynb>

--- notebooks/demo.py
+++ notebooks/demo.py
@@ -11,9 +11,13 @@ import marimo
 __generated_with = "0.23.1"
 app = marimo.App(width="medium", app_title="Demo")
 
+with app.setup:
+    import marimo as mo
+    import osmnx as ox
+
 
 @app.cell(hide_code=True)
-def _(mo):
+def _():
     mo.md(r"""
     # Demo
     """)
@@ -21,7 +25,7 @@ def _(mo):
 
 
 @app.cell(hide_code=True)
-def _(mo):
+def _():
     mo.md(r"""
     It's my first time using marimo. I want to create interactive elements first, as I need to know if this would suit my web app needs just fine.
     """)
@@ -30,28 +34,20 @@ def _(mo):
 
 @app.cell
 def _():
-    import marimo as mo
-    import osmnx as ox
-
-    return mo, ox
-
-
-@app.cell
-def _(mo):
     place = mo.ui.text(placeholder="Search for a city")
     place
     return (place,)
 
 
 @app.cell
-def _(ox, place):
+def _(place):
     G = ox.graph.graph_from_place(place.value, network_type="drive")
     G
     return (G,)
 
 
 @app.cell(hide_code=True)
-def _(mo):
+def _():
     mo.md(r"""
     Could this be?
     """)
@@ -59,13 +55,13 @@ def _(mo):
 
 
 @app.cell
-def _(G, ox):
+def _(G):
     fig, ax = ox.plot.plot_figure_ground(G, color="blue")
     return
 
 
 @app.cell(hide_code=True)
-def _(mo):
+def _():
     mo.md(r"""
     This is more akin to what I want:
     """)
@@ -73,9 +69,14 @@ def _(mo):
 
 
 @app.cell
-def _(G, ox):
+def _(G):
     ox.plot.plot_graph(G, node_size=0, show=False, edge_color="white")
+    return
 
+
+@app.cell
+def _(G):
+    ox.convert.graph_to_gdfs(G, nodes=False, ).explore(column="length", cmap="plasma", tiles="cartodbdarkmatter")
     return
 
 
